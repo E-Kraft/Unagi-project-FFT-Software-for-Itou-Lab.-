3af7ea350ba4ad1890059eef1b9213306fc602ba
E-Kraft <yudetama0711@gmail.com>
2025-11-22 17:41:30 +0900

    unification test

--- NMR.py
+++ NMR.py
@@ -1,5 +1,7 @@
 import numpy as np #FFTに使用します
 import struct #バイナリの読み込みに使用します
+import matplotlib.pyplot as plt #グラフ
+import openpyxl  as opx #エクセルの書き込みに使用
 import sys #システム諸々で使用します
 
 #今後追加予定の項目
@@ -240,3 +242,59 @@ def auto_const(fft_result,base_angle = np.pi / 100):
     shifted_re = shifted_data.real
     shifted_im = shifted_data.imag
     return (shifted_re,shifted_im,shifted_data)
+
+
+
+if __name__ == "__main__":
+    # ここは直接実行されたときだけ実行
+    # モジュールとしてインポートされた場合は実行されない(テストに用いる)
+    # __name__にはメインスクリプトとして実行された場合は__main__、モジュールとしてインポートされた場合はファイル名が代入される
+
+    #変数の定義
+    zeropoint = 1400 #ゼロポイント
+    n_ind= 3441 #切り取り範囲(負)
+    p_ind = 4752 #切り取り範囲(正)
+    base_angle = np.pi / 100 #基本の位相回転角(フーリエ変換ver2.5参照)　optionで選択できるようにするかも
+    file_path = '10k0613T2F.1010'
+
+    #-------------実行部分---------------------
+
+    #データの読み込み
+    raw_data = import_rawdata(file_path)
+
+    #周波数軸を生成
+    freq_column= raw_data.calc_freq()
+    #FFT実行
+    (fft_re,fft_im,fft_result) = raw_data.FFT(zeropoint)
+
+    #位相自動調整
+    (shifted_re,shifted_im,shifted_fft)= auto_const(fft_result)
+
+
+    #-------------エクセルに書き込み---------------------
+
+    wb = opx.Workbook() # workbookの作成
+    ws = wb.worksheets[0]
+
+    ws.cell(row=1, column=1, value="Frequency [kHz]")
+    for i,v in enumerate(freq_column[n_ind:p_ind],start=1):
+        ws.cell(row=i+1, column=1, value=v)
+
+    ws.cell(row=1, column=2, value="Real")
+    for i,v in enumerate(fft_re[n_ind:p_ind],start=1):
+        ws.cell(row=i+1, column=2, value=v)
+
+    ws.cell(row=1, column=3, value="Image")
+    for i,v in enumerate(fft_im[n_ind:p_ind],start=1):
+        ws.cell(row=i+1, column=3, value=v)
+
+    ws.cell(row=1, column=4, value="shifted_Real")
+    for i,v in enumerate(shifted_re[n_ind:p_ind],start=1):
+        ws.cell(row=i+1, column=4, value=v)
+
+    ws.cell(row=1, column=5, value="shifted_Image")
+    for i,v in enumerate(shifted_im[n_ind:p_ind],start=1):
+        ws.cell(row=i+1, column=5, value=v)
+
+
+    wb.save("fft_shift.xlsx") # Excelファイルの保存
